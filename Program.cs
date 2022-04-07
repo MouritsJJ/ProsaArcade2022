7f88b369e94814b9609c2307e3a3bf0f3bcd5161
MouritsJJ <mouritsjj@gmail.com>
2022-04-07 21:43:28 +0200

    Added logic in Main to run story teller

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ProsaDwarfs.Core:
+
+namespace ProsaDwarfs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IController c = new DwarfController();
+            
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine("This is a story about The Seven Dwarfs\n");
+
+                c.Start();
+
+                Console.WriteLine("The End!\n");
+
+                Console.Write("Would you like to meet The Seven Dwarfs again? [y/n] : ");
+                string? ans = Console.ReadLine();
+                if (ans.Trim().ToLower().StartsWith('n')) running = false;
+            }
+        }
+    }
+}
