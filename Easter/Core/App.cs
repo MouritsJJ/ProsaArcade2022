5e9d1a8ff8b277bafdc9f5d05caf95e2d62f39a9
MouritsJJ <mouritsjj@gmail.com>
2022-04-12 22:05:08 +0200

    SDL setup
    Added first windows setup
    Added core static SDL class
    Added app to contain relevant parameters
    Added simple program to testrun

---
+++ Easter/Core/App.cs
@@ -0,0 +1,10 @@
+using static SDL2.SDL;
+
+namespace Easter.Core
+{
+    public class App
+    {
+        public IntPtr Window { get; set; }
+        public IntPtr Renderer { get; set; }
+    }
+}
--- Easter/Program.cs
+++ Easter/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SDL2;
+using static Easter.Core.SDLHelper;
+using Easter.Core;
+
+namespace Easter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool running = true;
+            App app = new App();
+            Setup(app);
+
+            while (running)
+            {
+                PollEvents(ref running);
+                Render(app);
+            }
+
+            CleanUp(app);
+        }
+        
+    }
+}
